217537b3891f7eae713ec95a1dabbbf4564d3ff8
VictoriaDiro <victoriadiro01@gmail.com>
2019-04-22 21:03:33 +0200

    basic funcions ok

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,66 +3,59 @@
 const inputSearch = document.querySelector('.input__search');
 const searchButton = document.querySelector('.search__button');
 
-// const urlAPI = ;
+// El usuario mete el nombre de la serie en el input
+// Se recoje el texto que ha introducido el usuario en el input
+// Al hacer click en buscar la aplicación se conecta a la API
+// Se muestra en el resultado el título y la imagen
+// Si la serie no tiene imagen se mostrará 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+// El usuario puede indicar series favoritas
+// Al hacer click para hacer fav el color de fondo y la fuente cambian
+// Si se hace una nueva búsqueda los resultados de fav se acumulan a la izquierda
+// El linastado de favoritos se almacena en LocalStorage
 
-// LISTADO DE RESULTADOS
+function printTest() {
+  let urlAPI = `http://api.tvmaze.com/singlesearch/shows?q=${inputSearch.value}`;
+  fetch(urlAPI) 
+    .then(function(response) { console.log(response);
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
 
-fetch(`http://api.tvmaze.com/search/shows?q=${inputSearch.value}`)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    if(inputSearch.value !== '') {
-      // CREAR CONTENIDO EN EL DOM
-      function getTvShows() {
-        for(let i = 0; i < data.length; i++) {
-        // Crear elementos en HTML
-        const tvShowsContainer = document.querySelector('.tv__shows-container');
-        const newLi = document.createElement('li');
-        const newH2 = document.createElement('h2');
-        const newImg = document.createElement('img');
-        newImg.src = data[i].image;
-        newImg.alt = data[i].title;
-        // Añadir contenido a elementos de HTML
-        tvShowsContainer = document.createTextNode(data[i].title);
-        newH2.appendChild(tvShowsContainer);
-        newLi.appendChild(newH2);
-        news.appendChild(newLi);
-        newLi.appendChild(newImg);
-        // Añadir clases a elementos de HTML
-        newLi.classList.add('show__item');
-        newH2.classList.add('show__title');
-        newImg.classList.add('show__image');
+      let tvShowsContainer = document.querySelector('.tv__shows-container');
+      tvShowsContainer.innerHTML = '';
+      
+      const newLi = document.createElement('li');
+      newLi.classList.add('show__item');
+      tvShowsContainer.appendChild(newLi);
+
+      const newH2 = document.createElement('h2');
+      newH2.classList.add('show__title');
+      newLi.appendChild(newH2);
+      const newTitle = document.createTextNode(data.name);
+      newH2.appendChild(newTitle);
+
+      const newImg = document.createElement('img');
+      newImg.classList.add('show__image');
+      newLi.appendChild(newImg);
+      if(data.image === null) {
+        newImg.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
+      } else {
+        newImg.src = data.image.medium;
+        newImg.alt = data.title;
       }
-      searchButton.addEventListener('click', getTvShows);
-      newLi.innerHTML = data.result;
-    }
-  });
-
-// VARIABLE CON ARRAY DE FAVORITOS (A LA IZQ BAJO BUSCADOR)
-
-// FAVS SE ACUMULAN EN ARRAY
-
-// PETICIÓN AL SERVIDOR
-
-// CREAR CONTENIDO EN EL DOM
-
-// // API TVMAZE, SI NO HAY IMAGEN --> PLACEHOLDER
-// if(showz.image == '') {
-//   newImg.src = 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV';
-// }
+    });
+};
+searchButton.addEventListener('click', printTest);
 
 
-// // AL HACER CLICK, FONDO Y FUENTE SE CAMBIAN
-// function selectFavorite() {
-//   newLi.classList.toggle('select__favorite');
-// }
-// newLi.addEventListener('click', selectFavorite);
-
-// // FAVS EN LOCALSTORAGE
-// localStorage.setItem('name', 'imagen');
-// const name = localStorage.getItem('name', 'imagen');
-// console.log(name); //Ana
-
+// LLAMADA AL SERVIDOR
+// const promise = fetch(urlAPI)
+//   .then(function(response) {
+//     return response.json();
+//   })
+//   .then(function(data) {
+//     console.log(data);
+//   });
+// console.log(promise);
 //# sourceMappingURL=main.js.map
